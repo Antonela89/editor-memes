8976bc730286ef496dbb06ef0b02e49f25a6b2f8
Antonela <antonelaborgogno@gmail.com>
2023-09-27 15:08:29 -0300

    retoques className y configuracion para descarga de archivos

--- src/componentes/Imgmemes.jsx
+++ src/componentes/Imgmemes.jsx
@@ -9,18 +9,40 @@ const Imgmemes = () => {
         setTextmeme(e.target.value);
     }
 
+    const seleccionarImg = (e) => {
+        setImgmeme(e.target.value);
+    }
+
+    const descargar = (e) => {
+        html2canvas(document.querySelector("#exportar")).then(function(canvas) {
+            let img = canvas.toDataURL("memes/jpg");
+            let link = document.createElement("a");
+            link.download = "memeDescargado.jpg";
+            link.href = img;
+            link.click();
+        });
+    }
 
     return (
         <div className="text-center">
             <h1 className="mt-3 mb-3 text-center">Editor de memes</h1>
-
             <h3>Ingresa el texto del meme</h3>
             <input onChange={textomeme} className="form-control w-50 m-50 m-auto d-block" placeholder="Ingresa el texto superior" aria-label="" />
             
-            <figure className="text-center">
-                <p>{textmeme}</p>
-                <img  src="" alt=""/>
+            <h3 className="mt-3 mb-3 text-center">Elegí tu imagen favorita</h3>
+            <select onChange={seleccionarImg} className="form-select form-select-lg mb-3 w-50 m-auto" arial-label=".form-select-lg example">
+                <option value={1}>Futurama</option>
+                <option value={2}>Bob Esponja</option>
+                <option value={3}>Señora</option>
+                <option value={4}>Calarmardo</option>
+            </select>
+
+            <figure id="exportar" className="text-center">
+                <p className="w-100 px-30 position-absolute top-50 start-30 h1 text-center">{textmeme}</p>
+                <img src={`./memes/${imgmeme}.jpg`} alt="meme" className="figure-img mt-3 d-block m-auto"/>
             </figure>
+
+            <button onClick={descargar} type="button" className="btn btn-primary mt-4">Descargar meme</button>
         </div>
     )
 }
